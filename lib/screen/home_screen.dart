7a02e055f5f4e5d75633b100fe937120e54cd162
amb <mikaelh1651@gmail.com>
2023-02-19 22:46:20 +0900

    feat(home_screen): 위치 권한 받아와 빌드하는 기능 작성 및 마커 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,31 +8,50 @@ class HomeScreen extends StatelessWidget {
     126.921252,
   );
 
+  static final Marker marker = Marker(
+    markerId: MarkerId('company'),
+    position: companyLatLng,
+  );
+
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: renderAppBar(),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 2,
-            child: GoogleMap(
-              initialCameraPosition: CameraPosition(
-                target: companyLatLng,
-                zoom: 16,
-              ),
-            ),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: FutureBuilder<String>(
+        future: checkPermission(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData &&
+              snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.data == '위치 권한이 허가 되었습니다.') {
+            return Column(
               children: [
-                Icon(
-                  Icons.timelapse_outlined,
-                  color: Colors.blue,
-                  size: 50.0,
+                Expanded(
+                  flex: 2,
+                  child: GoogleMap(
+                    initialCameraPosition: CameraPosition(
+                      target: companyLatLng,
+                      zoom: 16,
+                    ),
+                    markers: Set.from([marker]),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.timelapse_outlined,
+                        color: Colors.blue,
+                        size: 50.0,
+                      ),
+                    ],
+                  ),
                 ),
                 const SizedBox(height: 20.0),
                 ElevatedButton(
@@ -39,24 +59,54 @@ class HomeScreen extends StatelessWidget {
                   child: Text('출근하기!'),
                 ),
               ],
+            );
+          }
+          return Center(
+            child: Text(
+              snapshot.data.toString(),
             ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
+
+  AppBar renderAppBar() {
+    return AppBar(
+      centerTitle: true,
+      title: Text(
+        '오늘도 출첵',
+        style: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+
+  Future<String> checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!isLocationEnabled) {
+      return '위치 서비스를 활성화해주세요.';
+    }
+
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if (checkedPermission == LocationPermission.denied) {
+      checkedPermission = await Geolocator.requestPermission();
+
+      if (checkedPermission == LocationPermission.denied) {
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+
+    if (checkedPermission == LocationPermission.deniedForever) {
+      return '앱의 위치 권한을 설정에서 허가해주세요.';
+    }
+
+    return '위치 권한이 허가 되었습니다.';
+  }
 }
 
-AppBar renderAppBar() {
-  return AppBar(
-    centerTitle: true,
-    title: Text(
-      '오늘도 출첵',
-      style: TextStyle(
-        color: Colors.blue,
-        fontWeight: FontWeight.w700,
-      ),
-    ),
-    backgroundColor: Colors.white,
-  );
-}
