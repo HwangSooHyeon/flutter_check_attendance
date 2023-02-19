e6421f0009487157bdc8a636469701757cb02787
amb <mikaelh1651@gmail.com>
2023-02-19 21:10:26 +0900

    feat(home_screen): scaffold의 appbar, body 작성

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
+  static final LatLng companyLatLng = LatLng(
+    37.5233273,
+    126.921252,
+  );
+
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: renderAppBar(),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(target: companyLatLng, zoom: 16),
+      ),
+    );
   }
+}
+
+AppBar renderAppBar() {
+  return AppBar(
+    centerTitle: true,
+    title: Text(
+      '오늘도 출첵',
+      style: TextStyle(
+        color: Colors.blue,
+        fontWeight: FontWeight.w700,
+      ),
+    ),
+    backgroundColor: Colors.white,
+  );
 }
